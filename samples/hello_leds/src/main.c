c2608fca03e03455fe86b6b307e47f7b02a34ae0
svhoy <sven.hoyer@hotmail.com>
2022-10-06 15:16:35 +0200

    update SIT led Example

--- samples/hello_leds/src/main.c
+++ samples/hello_leds/src/main.c
@@ -1,64 +1,59 @@
-
-#include <zephyr.h>
-#include <logging/log.h>
 #include <stdio.h>
-#include <drivers/gpio.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/gpio.h>
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 #define LED0_NODE 	DT_ALIAS(led0)
-#define LED0		DT_GPIO_LABEL(LED0_NODE, gpios)
-#define LED0_PIN	DT_GPIO_PIN(LED0_NODE, gpios)
 
 #define LED1_NODE 	DT_ALIAS(led1)
-#define LED1		DT_GPIO_LABEL(LED1_NODE, gpios)
-#define LED1_PIN	DT_GPIO_PIN(LED1_NODE, gpios)
 
 #define LED2_NODE 	DT_ALIAS(led2)
-#define LED2		DT_GPIO_LABEL(LED2_NODE, gpios)
-#define LED2_PIN	DT_GPIO_PIN(LED2_NODE, gpios)
 
 #define LED3_NODE 	DT_ALIAS(led3)
-#define LED3		DT_GPIO_LABEL(LED3_NODE, gpios)
-#define LED3_PIN	DT_GPIO_PIN(LED3_NODE, gpios)
 
 
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
 
 void main(void)
 {
+	printk("Welcome to SIT LED Test Example\n");
 	LOG_INF("Hello world leds sample");
 
-	const struct device *dev = device_get_binding(LED0);
-
-	gpio_pin_configure(dev, LED0_PIN, GPIO_OUTPUT_ACTIVE);
-	gpio_pin_configure(dev, LED1_PIN, GPIO_OUTPUT_ACTIVE);
-	gpio_pin_configure(dev, LED2_PIN, GPIO_OUTPUT_ACTIVE);
-	gpio_pin_configure(dev, LED3_PIN, GPIO_OUTPUT_ACTIVE);
-	gpio_pin_set(dev, LED0_PIN, 0);
-	gpio_pin_set(dev, LED1_PIN, 0);
-	gpio_pin_set(dev, LED2_PIN, 0);
-	gpio_pin_set(dev, LED3_PIN, 0);
+	gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
+	gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+	gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
+	gpio_pin_configure_dt(&led3, GPIO_OUTPUT_ACTIVE);
+	gpio_pin_set_dt(&led0, 0);
+	gpio_pin_set_dt(&led1, 0);
+	gpio_pin_set_dt(&led2, 0);
+	gpio_pin_set_dt(&led3, 0);
 	k_sleep(K_MSEC(1000));
-	gpio_pin_set(dev, LED0_PIN, 1);
-	gpio_pin_set(dev, LED1_PIN, 1);
-	gpio_pin_set(dev, LED2_PIN, 1);
-	gpio_pin_set(dev, LED3_PIN, 1);
+	gpio_pin_set_dt(&led0, 1);
+	gpio_pin_set_dt(&led1, 1);
+	gpio_pin_set_dt(&led2, 1);
+	gpio_pin_set_dt(&led3, 1);
 	k_sleep(K_MSEC(1000));
 
 	int count = 0;
 	while (1) {
 		LOG_INF("leds> hello %d",count++);
-		gpio_pin_set(dev, LED0_PIN, 0);
+		gpio_pin_set_dt(&led1, 0);
 		k_sleep(K_MSEC(1000));
-		gpio_pin_set(dev, LED0_PIN, 1);
-		gpio_pin_set(dev, LED1_PIN, 0);
+		gpio_pin_set_dt(&led1, 1);
+		gpio_pin_set_dt(&led0, 0);
 		k_sleep(K_MSEC(1000));
-		gpio_pin_set(dev, LED1_PIN, 1);
-		gpio_pin_set(dev, LED2_PIN, 0);
+		gpio_pin_set_dt(&led0, 1);
+		gpio_pin_set_dt(&led2, 0);
 		k_sleep(K_MSEC(1000));
-		gpio_pin_set(dev, LED2_PIN, 1);
-		gpio_pin_set(dev, LED3_PIN, 0);
+		gpio_pin_set_dt(&led2, 1);
+		gpio_pin_set_dt(&led3, 0);
 		k_sleep(K_MSEC(1000));
-		gpio_pin_set(dev, LED3_PIN, 1);
+		gpio_pin_set_dt(&led3, 1);
 	}
 }

6f6c16902687a23d77595c54b968103fe7e768e7
Sven <sven.hoyer@hotmail.com>
2024-02-08 08:29:23 +0100

    twr example and sit twr finished

--- samples/06_sit_ds_twr_responder/src/main.c
+++ samples/06_sit_ds_twr_responder/src/main.c
@@ -20,24 +20,21 @@
  *   along with Zephyr-DWM1001.  If not, see <https://www.gnu.org/licenses/>.
  * 
  */
-#include "deca_probe_interface.h"
-#include <config_options.h>
-#include <deca_device_api.h>
-#include <dw3000_hw.h> 
-#include <deca_spi.h>
-#include <example_selection.h>
-#include <port.h>
-#include <shared_defines.h>
+#include "deca_device_api.h"
 
-#include <sit.h>
-#include <sit_led.h>
+#include <sit/sit.h>
+#include <sit/sit_device.h>
+#include <sit/sit_distance.h>
+#include <sit/sit_config.h>
+#include <sit/sit_utils.h>
+#include <sit_led/sit_led.h>
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
-#define APP_NAME "SIMPLE TWR Responder EXAMPLE\n"
+#define APP_NAME "SIMPLE DS-TWR Responder EXAMPLE\n"
 
 /* Inter-ranging delay period, in milliseconds. */
 #define RNG_DELAY_MS 1000
@@ -66,62 +63,93 @@ static dwt_config_t config = {
 uint8_t this_initiator_node_id  = 1;
 uint8_t responder_node_id       = 2;
 
-/*rx twr_2_resp after tx twr_1_poll
- protected by responder's mp_request_at(twr_2_resp):POLL_RX_TO_RESP_TX_DLY_UUS
-*/
-#define POLL_TX_TO_RESP_RX_DLY_UUS 500
 
-/* Delay between frames, in UWB microseconds. See NOTE 1 below. */
-#define POLL_RX_TO_RESP_TX_DLY_UUS 650
+#define POLL_RX_TO_RESP_TX_DLY_UUS_T 900
+#define RESP_TX_TO_FINAL_RX_DLY_UUS_T 600
+#define FINAL_RX_TIMEOUT_T 1200
+#define PRE_TIMEOUT 5
 
 int main(void) {
 	printk(APP_NAME);
 	printk("==================\n");
 
-	int init_ok = sit_init(&config, TX_ANT_DLY, RX_ANT_DLY);
-	// INIT LED and let them Blink one Time to see Intitalion Finshed
+    // INIT LED and let them Blink one Time to see Intitalion Finshed
     sit_led_init();
-    
+
+	int init_ok = 0;
+    do {
+		init_ok = sit_init();
+	} while (init_ok > 1);
+
     if(init_ok < 0){
-        sit_set_led(2, 0);
+        sit_set_led(2, 1);
     } else {
-        sit_set_led(1, 0);
+        sit_set_led(1, 1);
     }
 
-    uint32_t regStatus = sit_get_device_status();
-    LOG_INF("statusreg = 0x%08x",regStatus);
-    k_sleep(K_SECONDS(2)); // Allow Logging to write out put 
-
-    int frame_sequenz = 0;
-    k_yield();
+    uint8_t frame_sequenz = 0;
 
 	while (1) {
-		regStatus = sit_get_device_status();
-		LOG_INF("sequence(%u) starting ; statusreg = 0x%08x",frame_sequenz,regStatus);
-		sit_receiveNow(0);
-        msg_header_t rx_poll_msg;
+		sit_receive_now();
+        msg_simple_t rx_poll_msg;
 		msg_id_t msg_id = twr_1_poll;
-		if(sit_checkReceivedIdMsg(msg_id, &rx_poll_msg)){
+		if(sit_check_msg_id(msg_id, &rx_poll_msg)){
 			uint64_t poll_rx_ts = get_rx_timestamp_u64();
             
-            uint32_t resp_tx_time = (poll_rx_ts + (POLL_RX_TO_RESP_TX_DLY_UUS * UUS_TO_DWT_TIME)) >> 8;
-
-            uint32_t resp_tx_ts = (((uint64_t)(resp_tx_time & 0xFFFFFFFEUL)) << 8) + TX_ANT_DLY;
+            uint32_t resp_tx_time = (poll_rx_ts + (POLL_RX_TO_RESP_TX_DLY_UUS_T * UUS_TO_DWT_TIME)) >> 8;
             
-            msg_ss_twr_final_t msg_ss_twr_final_t = {
-                    ss_twr_2_resp, (uint8_t)(rx_poll_msg.header.sequence + 1),
-                    rx_poll_msg.header.dest , rx_poll_msg.header.source,(uint32_t)poll_rx_ts, resp_tx_ts,0
+            msg_simple_t msg_ds_poll_resp = {
+                    ds_twr_2_resp,
+                    rx_poll_msg.header.sequence,
+                    rx_poll_msg.header.dest,
+                    rx_poll_msg.header.source,
+                    0
                 };
-            sit_sendAt((uint8_t*)&msg_ss_twr_final_t, sizeof(msg_ss_twr_final_t), resp_tx_time);
+            sit_set_rx_after_tx_delay(RESP_TX_TO_FINAL_RX_DLY_UUS_T);
+            sit_set_rx_timeout(FINAL_RX_TIMEOUT_T);
+            sit_set_preamble_detection_timeout(PRE_TIMEOUT);
+            bool ret = sit_send_at_with_response((uint8_t*)&msg_ds_poll_resp, sizeof(msg_simple_t), resp_tx_time);
+            if (ret == false) {
+                continue;
+                LOG_WRN("Something is wrong with Sending Poll Resp Msg");
+            }
+            msg_ds_twr_final_t rx_ds_final_msg;
+		    msg_id = ds_twr_3_final;
+            if(sit_check_ds_final_msg_id(msg_id, &rx_ds_final_msg)){
+                uint64_t resp_tx_ts = get_tx_timestamp_u64();
+                uint64_t final_rx_ts = get_rx_timestamp_u64();
+
+                uint32_t poll_tx_ts = rx_ds_final_msg.poll_tx_ts;
+                uint32_t resp_rx_ts = rx_ds_final_msg.resp_rx_ts;
+                uint32_t final_tx_ts = rx_ds_final_msg.final_tx_ts;
+
+                uint32_t poll_rx_ts_32, resp_tx_ts_32, final_rx_ts_32;
+                poll_rx_ts_32 = (uint32_t) poll_rx_ts;
+                resp_tx_ts_32 = (uint32_t) resp_tx_ts;
+                final_rx_ts_32 = (uint32_t) final_rx_ts;
+
+                double Ra, Rb, Da, Db;
+                int64_t tof_dtu;
+                Ra = (double)(resp_rx_ts - poll_tx_ts);
+                Rb = (double)(final_rx_ts_32 - resp_tx_ts_32);
+                Da = (double)(final_tx_ts - resp_rx_ts);
+                Db = (double)(resp_tx_ts_32 - poll_rx_ts_32);
+                tof_dtu = (int64_t)((Ra * Rb - Da * Db) / (Ra + Rb + Da + Db));
+
+                double tof = tof_dtu * DWT_TIME_UNITS;
+                double distance = tof * SPEED_OF_LIGHT;
+                LOG_INF("Distance: %lf", distance);
+            } else {
+                LOG_WRN("Something is wrong with Final Msg Receive");
+                dwt_writesysstatuslo(SYS_STATUS_ALL_RX_TO | SYS_STATUS_ALL_RX_ERR);
+            }
 
 		} else {
-			LOG_WRN("Something is wrong");
+			LOG_WRN("Something is wrong with Poll Msg Receive");
             dwt_writesysstatuslo(SYS_STATUS_ALL_RX_TO | SYS_STATUS_ALL_RX_ERR);
 		}
-		regStatus = sit_get_device_status();
-		LOG_INF("sequence(%u) ending ; statusreg = 0x%08x",frame_sequenz,regStatus);
 		frame_sequenz++;
-        k_sleep(K_MSEC(RNG_DELAY_MS));
+        k_msleep(80);
 	}
 
     return 0;
